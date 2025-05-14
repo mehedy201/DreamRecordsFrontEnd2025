95ce944233a53efffd9887bd1ae11d89c29cfa5a
mehedy201 <mehedy643@gmail.com>
2025-05-14 19:06:45 +0600

    updated userProgile page with dynamic data

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -4,36 +4,9 @@ import "./Profile.css";
 import * as Dialog from "@radix-ui/react-dialog";
 import { FaCamera } from "react-icons/fa";
 import Modal from "../../components/Modal";
-const personalInfo = [
-  { title: "Address:" },
-  { label: "Registered As:", value: "Individual" },
-  { label: "First Name:", value: "John" },
-  { label: "Last Name:", value: "Doe" },
-  { label: "Phone:", value: "+91 8001134466" },
-];
-const Address = [
-  { title: "Address" },
-  {
-    label: "Address Line 1:",
-    value: "H.No 10 Ward No. Jharpuri Road Dungeja",
-  },
-  { label: "Address Line 2:", value: "Pinanagwan, Punhana, Nuh" },
-  { label: "Postal Code:", value: "122508" },
-  { label: "City:", value: "Gurgaon" },
-  { label: "State:", value: "Haryana" },
-  { label: "Country:", value: "India" },
-];
-const LabelInfo = [
-  { title: "Label Info" },
-  { label: "Channel Name:", value: "AKMDigital" },
-  {
-    label: "Channel URL:",
-    value:
-      "https://open.spotify.com/?flow_ctx=7aa5a067-6601-4e29-9794-07af35e39eee%3A1738420603",
-  },
-  { label: "Subscriber Count:", value: "124616956" },
-  { label: "Videos Count:", value: "92357" },
-];
+import { useSelector } from "react-redux";
+
+
 const security = [
   { title: "Security Info" },
 
@@ -46,6 +19,10 @@ const security = [
 
 function Profile() {
   const [image, setImage] = useState(null);
+
+  // const dispatch = useDispatch();
+  const userData = useSelector((state) => state.userData.userData);
+  console.log(userData)
 
   // Handle Image Upload
   const handleImageChange = (event) => {
@@ -69,7 +46,7 @@ function Profile() {
           ) : (
             <>
               <img
-                src="src/assets/artists/artist1.png"
+                src={userData?.image}
                 alt="Profile"
                 onClick={() => document.getElementById("fileInput").click()}
               />
@@ -88,59 +65,93 @@ function Profile() {
           )}
         </div>
         <div className="profile-img-txt">
-          <h1>John Doe</h1>
-          <h3>johndoe</h3>
+          <h1>{userData?.first_name} {userData?.last_name}</h1>
+          <h3>{userData?.userName}</h3>
         </div>
       </Flex>
+      {/* Personal Information ________________________________ */}
       <div className="profile-info">
-        {personalInfo.map((item, index) => (
-          <div key={index}>
-            <h5>{item.title}</h5>
-            <div className="d-flex">
-              <p>{item.label}</p>
-              <p className="profile-value-text ">{item.value}</p>
-            </div>
-          </div>
-        ))}
+        <h5>Personal Information</h5>
+        <div style={{marginTop: '14px'}} className="d-flex">
+          <p>Registered As:</p>
+          <p className="profile-value-text ">{userData?.roll == "User" ? "Individual" : userData?.roll}</p>
+        </div>
+        <div className="d-flex">
+          <p>First Name:</p>
+          <p className="profile-value-text ">{userData?.first_name}</p>
+        </div>
+        <div className="d-flex">
+          <p>Last Name:</p>
+          <p className="profile-value-text ">{userData?.last_name}</p>
+        </div>
+        <div className="d-flex">
+          <p>Phone:</p>
+          <p className="profile-value-text ">{userData?.phone}</p>
+        </div>
       </div>
+
+      {/* Address ________________________________ */}
       <div className="profile-info">
-        {Address.map((item, index) => (
-          <div key={index}>
-            <h5>{item.title}</h5>
+          <div>
+            <h5>Address</h5>
+            <div style={{marginTop: '14px'}} className="d-flex">
+              <p>Address Line 1:</p>
+              <p className="profile-value-text">{userData?.address}</p>
+            </div>
             <div className="d-flex">
-              <p>{item.label}</p>
-              <p className="profile-value-text">{item.value}</p>
+              <p>Address Line 1:</p>
+              <p className="profile-value-text">{userData?.address2}</p>
+            </div>
+            <div className="d-flex">
+              <p>Postal Code:</p>
+              <p className="profile-value-text">{userData?.postalCode}</p>
+            </div>
+            <div className="d-flex">
+              <p>City:</p>
+              <p className="profile-value-text">{userData?.city}</p>
+            </div>
+            <div className="d-flex">
+              <p>State:</p>
+              <p className="profile-value-text">{userData?.state?.name}</p>
+            </div>
+            <div className="d-flex">
+              <p>Country:</p>
+              <p className="profile-value-text">{userData?.country?.name}</p>
             </div>
           </div>
-        ))}
       </div>
 
       <div className="row profile-download-row">
         <div className="col-6">
           <div className="profile-info">
-            {LabelInfo.map((item, index) => (
-              <div key={index}>
-                <h5>{item.title}</h5>
-
-                <div className="d-flex">
-                  <p className="profile-downloadRow-label">{item.label}</p>
-                  {item.label === "Channel URL:" ? (
-                    <a
-                      href={item.value}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="profile-value-text"
-                    >
-                      {item.value.slice(0, 35) + "..."}
-                    </a>
-                  ) : (
-                    <p className="profile-value-text">{item.value}</p>
-                  )}
+            <h5>Label Info</h5>
+            {/* {userData?.label &&  */}
+                <div style={{marginTop: '14px'}}>
+                  <div className="d-flex">
+                    <p className="profile-downloadRow-label">Label Name:</p>
+                    <p className="profile-value-text">demo</p>
+                  </div>
+                  <div className="d-flex">
+                    <p className="profile-downloadRow-label">Channel Name:</p>
+                    <p className="profile-value-text">demo</p>
+                  </div>
+                  <div className="d-flex">
+                    <p className="profile-downloadRow-label">Channel URL:</p>
+                    <p className="profile-value-text">demo</p>
+                  </div>
+                  <div className="d-flex">
+                    <p className="profile-downloadRow-label">Subscriber Count:</p>
+                    <p className="profile-value-text">1</p>
+                  </div>
+                  <div className="d-flex">
+                    <p className="profile-downloadRow-label">Videos Count:</p>
+                    <p className="profile-value-text">11</p>
+                  </div>
                 </div>
-              </div>
-            ))}
+            {/* } */}
           </div>
         </div>
+        {/* Govorment Document _________________________________ */}
         <div className="col-6">
           <div className="profile-info" style={{ marginLeft: "16px" }}>
             <h5 style={{ marginBottom: "10px" }}>Documents</h5>
@@ -160,31 +171,23 @@ function Profile() {
                 <button>Download</button>
               </div>
             </div>
-            {/* <div>
-                <span>Agreement</span>
-                <div
-                  className="profile-info-download-div"
-                  style={{ marginRight: "0" }}
-                >
-                  <p>Front Page.PDF</p>
-                  <button>Download</button>
-                </div>
-              </div> */}
           </div>
         </div>
       </div>
       <div className="profile-info d-flex">
         <div style={{ width: "80%" }}>
-          {security.map((item, index) => (
-            <div key={index}>
-              <h5>{item.title}</h5>
+            <div >
+              <h5>Security Info</h5>
 
+              <div style={{marginTop: '14px'}} className="d-flex">
+                <p>Email:</p>
+                <p className="profile-value-text">{userData?.email}</p>
+              </div>
               <div className="d-flex">
-                <p>{item.label}</p>
-                <p className="profile-value-text">{item.value}</p>
+                <p>Password:</p>
+                <p className="profile-value-text">*********</p>
               </div>
             </div>
-          ))}
         </div>
         <Dialog.Root>
           <Dialog.Trigger className="profile-pass-btn">
