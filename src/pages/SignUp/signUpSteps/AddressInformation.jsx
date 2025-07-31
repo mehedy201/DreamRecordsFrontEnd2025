122536301844fac93a6c41b39c3b8634af8e68d3
mehedy201 <mehedy643@gmail.com>
2025-07-31 13:49:12 +0600

    The Address Information page is now correctly getting the value of the input field and the country state, suitable for connecting to the API.

--- src/pages/SignUp/signUpSteps/AddressInformation.jsx
+++ src/pages/SignUp/signUpSteps/AddressInformation.jsx
@@ -1,54 +1,103 @@
+import { useState } from 'react';
 import Dropdown from '../../../components/Dropdown';
+import { CountrySelect, StateSelect } from 'react-country-state-city';
+import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+import { setAddressInformation, setStep } from '../../../redux/features/signUpDataHandleSlice/signUpDataHandleSlice';
 
 const AddressInformation = () => {
+
+    const dispatch = useDispatch();
+    const {step, addressInformation} = useSelector(state => state.signUpData);
+
+    // Country State Select 
+    const [countryid, setCountryid] = useState(0);
+    const [country, setCountry] = useState(addressInformation?.country);
+    const [state, setState] = useState(addressInformation?.state)
+    const [countryError, setCountryError] = useState('');
+    const [stateError, setStateError] = useState('')
+
+
+    const [loading, setLoading] = useState(false)
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => {
+      setLoading(true)
+      if(!country){
+        setCountryError('Please select your Country')
+      }
+      if(!state){
+        setStateError('Please select your State')
+      }
+      const payload = {...data, country, state}
+      dispatch(setAddressInformation(payload))
+      dispatch(setStep(step +1))
+      console.log(payload)
+      // navigate('/email-verification')
+    };
+
+
+    const handlePrev = () => {
+      dispatch(setStep(step-1))
+    }
+
     return (
         <div>
-            <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <label htmlFor="">Address Line 1 *</label>
-                <input
-                  type="text"
-                  name="addressLine1"
-                />
+                <input type="text" {...register("addressLine1", { required: true })} />
+                {errors.addressLine1 && <p style={{color: 'red', marginTop: '-10px'}}>Address Line 1 Required</p>}
+
                 <label htmlFor="">Address Line 2</label>
-                <input
-                  type="text"
-                  name="addressLine2"
-                />
+                <input type="text" {...register("addressLine2")} />
+
                 <div className="signUp-form-grid">
                   <div>
                     <label htmlFor="">Select Country *</label>
-                    <Dropdown
-                      label="India"
-                      options={["Option 1", "Option 2", "Option 3"]}
-                      className="signUp-dropdown-trigger"
+                    <CountrySelect
+                        onChange={(e) => {
+                            setCountryid(e.id);
+                            const name = e.name;
+                            const emoji = e.emoji;
+                            const v = {name, emoji}
+                            setCountry(v)
+                        }}
+                        defaultValue={country}
+                        placeHolder="Select Country"
                     />
+                    {
+                        countryError && <p className="text-red-600 pb-2">{countryError}</p>
+                    }
                   </div>
                   <div>
                     <label htmlFor="">Select State *</label>
-                    <Dropdown
-                      label="West Bengal"
-                      options={["Option 1", "Option 2", "Option 3"]}
-                      className="signUp-dropdown-trigger"
+                    <StateSelect
+                        countryid={countryid}
+                        onChange={(e) => {
+                            setState(e)
+                        }}
+                        defaultValue={state}
+                        placeHolder="Select State"
                     />
+                    {
+                        stateError && <p className="text-red-600 pb-2">{stateError}</p>
+                    }
                   </div>
                   <div>
                     <label htmlFor="">City *</label>
-
-                    <input
-                      type="text"
-                      name="state"
-                    />
+                    <input type="text" {...register("city", { required: true })} />
+                    {errors.city && <p style={{color: 'red', marginTop: '-10px'}}>City Required</p>}
                   </div>
                   <div>
                     <label htmlFor="">Postal Code *</label>
-                    <input
-                      type="text"
-                      name="postalCode"
-                      className="input-field"
-                    />
+                    <input type="text" {...register("postalCode", { required: true })} />
+                    {errors.postalCode && <p style={{color: 'red', marginTop: '-10px'}}>Postal Code Required</p>}
                   </div>
                 </div>
-            </div>
+                <div className="signUp-buttons">
+                    <button className="theme-btn2" onClick={handlePrev}>Back</button>
+                    <button className="signUp-next-btn" type='submit'>Next</button>
+                </div>
+            </form>
         </div>
     );
 };
