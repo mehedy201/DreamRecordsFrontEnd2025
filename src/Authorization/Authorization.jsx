3dcd68bb865f84b8380d50c70a2cf2d7389562bf
mehedy201 <mehedy643@gmail.com>
2025-08-06 00:02:04 +0600

    update authorization last login api issue add defult object as a body

--- src/Authorization/Authorization.jsx
+++ src/Authorization/Authorization.jsx
@@ -65,9 +65,7 @@ const Authorization = ({ children }) => {
         //     navigate('/sign-up-address-info')
         //     return
         // }
-        await axios.patch(
-          `https://dream-records-2025-m2m9a.ondigitalocean.app/api/v1/users/last-log-in/${userId}`
-        );
+        await axios.patch(`https://dream-records-2025-m2m9a.ondigitalocean.app/api/v1/users/last-log-in/${userId}`, {});
       } catch (err) {
         console.error("Auth check failed:", err.message);
         // localStorage.removeItem("token");
